2b2a3ae99bbfff2543a43de8d3e74c47126d6dd8
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2022-01-18 22:49:33 -0500

    feat: problem 7 solution

--- src/solutions.js
+++ src/solutions.js
@@ -459,8 +459,57 @@ function formatCoords(...values) {
  ******************************************************************************/
 
 function mimeFromFilename(filename) {
-  // Replace this comment with your code...
-  // NOTE: Use a switch statement in your solution.
+  const ext = filename.substr(filename.lastIndexOf('.')).replace('.', '');
+
+  switch (ext) {
+    case 'html':
+    case 'htm':
+      return 'text/html';
+    case 'css':
+      return 'text/css';
+    case 'js':
+      return 'text/javascript';
+    case 'jpg':
+    case 'jpeg':
+      return 'image/jpeg';
+    case 'gif':
+      return 'image/gif';
+    case 'bmp':
+      return 'image/bmp';
+    case 'ico':
+    case 'cur':
+      return 'image/x-icon';
+    case 'png':
+      return 'image/png';
+    case 'svg':
+      return 'image/svg+xml';
+    case 'webp':
+      return 'image/webp';
+    case 'mp3':
+      return 'audio/mp3';
+    case 'wav':
+      return 'audio/wav';
+    case 'mp4':
+      return 'video/mp4';
+    case 'webm':
+      return 'video/webm';
+    case 'json':
+      return 'application/json';
+    case 'mpeg':
+      return 'video/mpeg';
+    case 'csv':
+      return 'text/csv';
+    case 'ttf':
+      return 'font/ttf';
+    case 'woff':
+      return 'font/woff';
+    case 'zip':
+      return 'application/zip';
+    case 'avi':
+      return 'video/x-msvideo';
+    default:
+      return 'application/octet-stream';
+  }
 }
 
 /*******************************************************************************
